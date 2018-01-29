e2d1eb621882aa55ac854000d82916561ea841e1
DoNook <recall2300@gmail.com>
2018-01-29 07:47:32 +0000

    Create 123123.c

---
+++ 123123.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+// 2557#if 0
+
+int main(){ printf("Hello World!");}
+#endif
+// 1000#if 0
+int main(){ int a, b; scanf("%d", &a); scanf("%d", &b); printf("%d",a+b);}
+#endif
+//1001#if 0
+int main(){ int a, b; scanf("%d", &a); scanf("%d", &b); printf("%d", a - b);}
+#endif
+//7287#if 0
+int main(){ int a, b; printf("%d\n",84); printf("%s\n","recall2300");}
+#endif
+//10172#if 0
+int main(){ printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");}
+#endif
+// 10718#if 0
+int main(){ printf("%s", "강한친구 대한육군\n"); printf("%s", "강한친구 대한육군");}
+#endif
+// 11718#if 0
+int main(){ char c; for (; ~scanf("%c", &c);)printf("%c", c);}
+#endif
+// 11719#if 0
+int main(){ char c; for (; ~scanf("%c", &c);)printf("%c", c);}
+#endif
+// 1000#if 0
+
+int main(){ int a, b; scanf(" %d %d ", &a, &b); printf("%d\n",a+b);}
+#endif
+// 1001#if 0
+int main(){ int a, b; scanf(" %d %d ", &a, &b); printf("%d\n", a - b);}
+#endif
+// 10998#if 0
+int main(){ int a, b; scanf(" %d %d ", &a, &b); printf("%d\n", a * b);}
+#endif
+// 1008#if 0
+int main(){ int a, b; scanf("%d %d", &a, &b); printf("%.15lf\n", ((double)a / (double)b));}
+#endif
+// 10869#if 0
+int main(){ int a, b; scanf("%d %d", &a, &b); printf("%d\n", a + b); printf("%d\n", a - b); printf("%d\n", a * b); printf("%d\n", (int)((double)a / (double)b)); printf("%d\n", a % b);}
+#endif
+// 10430#if 0int main(){ int a, b, c; scanf("%d %d %d", &a, &b, &c); printf("%d\n", (a + b) % c); printf("%d\n", (a%c + b%c) % c); printf("%d\n", (a * b) % c); printf("%d\n", (a%c * b%c) % c);}
+#endif
+// 2558#if 0int main(){ int a,b;
+ scanf("%d %d",&a,&b); printf("%d", a + b);}
+#endif
+// 2839#if 0int main(){ int input, t=0,f=0; scanf("%d", &input); while (input % 5)  input -= 3, ++t; f = input / 5; printf("%d\n", input < 0 ? -1 : t + f);}
+#endif
+// 2741#if 1int main(){ int a,i; scanf("%d", &a); for (i = 1; i <= a; i++) {  printf("%d\n", i); }}
+#endif
+// 2742#if 0int main(){ int a; scanf("%d", &a); while (a > 0) {  printf("%d\n", a--); }}
+#endif
+// 2739#if 0int main(){ int a,i; scanf("%d", &a); for (i = 1; i <= 9; i++) {  printf("%d * %d = %d\n", a, i, a*i); } }
+#endif
+// 2438#if 0int main(){ int a, i,j; scanf("%d", &a); for (i = 1; i <= a; i++) {  for (j = 1; j <= i; j++)  {   printf("*");  }  printf("\n"); }}
+#endif
+// 2439#if 0int main(){ int a, i,j,k; scanf("%d", &a); for (i = 1; i <= a; i++) {  for (k = a-i; k > 0; k--)  {   printf(" ");  }  for (j = 1; j <= i; j++)  {   printf("*");  }  printf("\n"); }}
+#endif
+// 2440#if 0int main(){ int a, i, j, k; scanf("%d", &a); for (i = 1; i <= a; i++) {  for (k = a - i; k > 0; k--)  {   printf(" ");  }  for (j = 1; j <= i; j++)  {   printf("*");  }  printf("\n"); }}
+#endif
